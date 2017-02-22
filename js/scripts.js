b35e621a52f48b10ec3b9d8a4f2a7ecf67bf207b
Emheff <eheffernan@vcd-ceb131-21.eastern.ewu.edu>
2017-02-22 09:33:53 -0800

    Google Doodle
    started google doodle web page for project 2

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// YOUR SCRIPTS GO HERE
+// Randomize Image
+// Store Image URLS
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// Get Random Number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+// Show Images
+var tmp = $('img').attr('src', eval('img' + numRand) );
+console.log(tmp)
