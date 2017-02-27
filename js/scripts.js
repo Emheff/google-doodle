3db3a6f2969e5dad1403fb1af67426b1c317b64a
Emheff <eheffernan@vcd-ceb131-21.eastern.ewu.edu>
2017-02-27 09:54:48 -0800

    added animation
    added animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Store 3 images
-var img0 = '../img/200.jpg';
+var img0 = '../img/400.jpg';
 var img1 = '../img/300.jpg';
-var img2 = '../img/400.jpg';
+var img2 = '../img/200.jpg';
 
 // Get random number
 var max = 3;
@@ -10,4 +10,16 @@ console.log(numRand);
 
 // Display randomized images
 $('#random').attr( 'src', eval('img' + numRand) );
+
+// Hide square
+    $('#stage.svg').hide();
+// click on button
+$('button').click(function() {
+                  
+
+// show square
+    $('#stage.svg').show('slow');
+    console.log('Ok')
+});
+
 
