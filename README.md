0725c82fb91c3dff9e56ead0d0089bd5e0098ec2
Emheff <emilyheffernan20@eagles.ewu.edu>
2017-02-26 22:51:51 -0800

    fixed google doodle
    Fixed problems with website, and added svg images

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 ## Brainstorm Ideas
 
-* ABC
-* XYZ
+* Cats
+* Jungle
+* Balloons
 
 
 ## Content Doodle
 
 Your content goes here. 
-![Google Doodle](https://www.google.com/logos/2012/d4g_poland12-hp.jpg)
+![Google Doodle]
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
-// YOUR SCRIPTS GO HERE
-// Randomize Image
-// Store Image URLS
+// Store 3 images
 var img0 = '../img/200.jpg';
 var img1 = '../img/300.jpg';
 var img2 = '../img/400.jpg';
 
-// Get Random Number
+// Get random number
 var max = 3;
 var numRand = Math.floor( Math.random() * max );
 console.log(numRand);
 
-// Show Images
-// var tmp = $('img').attr('src', eval('img' + numRand) );
-console.log(tmp)
+// Display randomized images
+$('#random').attr( 'src', eval('img' + numRand) );
+
